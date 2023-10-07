7ae6c77a9ef3ca3e94faa80ef3b4b41cc61c1394
brobertson3 <brobertson3@users.noreply.github.com>
2023-10-07 16:13:52 -0700

    Setup Unit Testing in app

---
+++ src/tests/App.test.tsx
@@ -0,0 +1,23 @@
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/react'
+import App from '../components/app/App'
+
+beforeEach(() => {
+  // IntersectionObserver isn't available in test environment
+  const mockIntersectionObserver = jest.fn();
+  mockIntersectionObserver.mockReturnValue({
+    observe: () => null,
+    unobserve: () => null,
+    disconnect: () => null
+  });
+  window.IntersectionObserver = mockIntersectionObserver;
+});
+
+test('demo', () => {
+  expect(true).toBe(true)
+})
+
+test("Renders the main app component", () => {
+  render(<App />)
+  expect(true).toBeTruthy()
+})
