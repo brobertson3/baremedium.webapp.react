92ca86951139e13051b4225d12af575c4f745f3d
brobertson3 <brobertson3@users.noreply.github.com>
2023-10-09 12:02:55 -0700

    Changed the file structure to work with Jest

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,6 @@ export default {
   // process `*.tsx` files with `ts-jest`
   },
   moduleNameMapper: {
-      '\\.(gif|ttf|eot|svg|jpg|jpeg|png|css)$': '<rootDir>/src/tests/mocks/fileMock.js',
+      '\\.(gif|ttf|eot|svg|jpg|jpeg|png|css)$': '<rootDir>/src/test/__mocks__/fileMock.js',
   },
 }
